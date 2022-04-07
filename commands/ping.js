38926ac891ab7126f4f12b55eedee394a1aba4bb
beans-squared <starskillz13@gmail.com>
2022-04-07 13:04:24 -0500

    Updated ping command to actually display roundtrip latency

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
     .setName("ping")
     .setDescription("Replies with pong"),
   async execute(interaction) {
-    await interaction.reply("Pong!");
+    const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
+    await interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
 };

6c98fc254a998245c7d40e62e62c9a130a9b3ad0
beans-squared <starskillz13@gmail.com>
2022-04-11 16:26:12 -0500

    Fixed API error on DM

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,11 @@
 module.exports = {
   name: "messageCreate",
   execute(message) {
+    if(message.author.id === "959478879137980416") return;
+
     if(message.channel.type === "DM"){
-        // TODO figure out why this triggers DiscordAPIError: Cannot send messages to this user
         console.log(`Received a DM from ${message.author.tag}`);
-        message.author.send(`Hey there, ${message.author.username}!\n\nMy name is Gunther, and I am a work in progress, custom-built discord bot. If you'd like to know more about me, check back later and I may have more information for you!\n\nHave a nice day!`).catch(console.error);
+        message.author.send(`Hey there, ${message.author.username}!\n\nMy name is Gunther, and I am a work in progress, custom-built discord bot. If you'd like to know more about me, check back later and I may have more information for you!\n\nHave a nice day!`);
     } else if(message.channel.type === "GUILD_TEXT"){
       if(message.content.includes("Gunther!")){
         message.channel.send("Hey!");

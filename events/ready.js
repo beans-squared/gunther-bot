a808aa8b2320bf664cafdb69e4be56bc6f316e77
beans-squared <starskillz13@gmail.com>
2022-04-22 17:16:55 -0500

    Added regularly changing presence types

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,64 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setPresence({
-			activities: [{
-				name: 'DM me for info!',
-			}],
-			status: 'online',
-		});
+
+		// Gunther's presence update cycle
+		decideActivity();
+		setInterval(decideActivity, 1800000);
+
+		function decideActivity() {
+			const activityChoice = Math.floor(Math.random() * 3);
+			console.log('Setting new activity status...');
+
+			switch (activityChoice) {
+			case 0: {
+				console.log('Playin a game was chosen');
+				const games = [
+					'Besiege',
+					'Deep Rock Galactic',
+					'Dyson Sphere Program',
+					'Factorio',
+					'Minecraft',
+					'Hydroneer',
+					'RimWorld',
+					'Satisfactory',
+					'Stardew Valley',
+					'Subnautica',
+					'Teardown',
+					'Terraria',
+					'The Forest',
+				];
+				const gameChoice = Math.floor(Math.random() * games.length);
+				client.user.setPresence({
+					activities: [{
+						type: 'PLAYING',
+						name: `${games[gameChoice]}`,
+					}],
+					status: 'online',
+				});
+				break;
+			}
+			case 1:
+				console.log('Listening to Spotify was chosen');
+				client.user.setPresence({
+					activities: [{
+						type: 'LISTENING',
+						name: 'Spotify',
+					}],
+					status: 'online',
+				});
+				break;
+			case 2:
+				console.log('Watching YouTube was chosen');
+				client.user.setPresence({
+					activities: [{
+						type: 'WATCHING',
+						name: 'YouTube',
+					}],
+					status: 'online',
+				});
+				break;
+			}
+		}
 	},
 };

33d00bf3eb9931116d03452fdf535c062b3f5adb
beans-squared <starskillz13@gmail.com>
2022-06-04 14:41:06 -0500

    Temporary restriction of commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,6 +5,9 @@ module.exports = {
 	execute(interaction) {
 		// Slash command interactions
 		if (interaction.isCommand()) {
+			// Temporary restriction to developer only for all commands
+			if (interaction.user.id != '240161761393639425') return interaction.reply({ content: 'Commands are currently restricted to developers of this application only.', ephemeral: true });
+
 			logger.debug(`Command: /${interaction.commandName} has been executed by ${interaction.user.tag} in #${interaction.channel.name}`);
 
 			const command = interaction.client.commands.get(interaction.commandName);
